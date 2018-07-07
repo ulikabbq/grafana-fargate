850bee05ddcf433279c604815f36c2b6a4410d90
ulikabbq <robmarlow@gmail.com>
2018-07-06 23:24:00 -0500

    update data ami provider

--- tf/bastion.tf
+++ tf/bastion.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-16.06-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
   }
 
   filter {
