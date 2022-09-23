b454b618a9c6a2f250665389a202d47c10ef694c
yaz-saleh <74604867+yaz-saleh@users.noreply.github.com>
2022-09-23 09:05:10 -0400

    Clean up

--- tf/ssm.tf
+++
@@ -1,8 +0,0 @@
-#resource "aws_ssm_parameter" "GF_INSTALL_PLUGINS" {
-#  name  = "/grafana/GF_INSTALL_PLUGINS"
-#  type  = "String"
-#  value = "grafana-worldmap-panel,grafana-clock-panel,jdbranham-diagram-panel,natel-plotly-panel"
-#}
-
-
-
